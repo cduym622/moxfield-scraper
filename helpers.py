70b5c064b31892ee1541593b8884bf820c34a2fc
Chris <cduym622@gmail.com>
2024-10-18 00:13:45 -0400

    added better documentation and fixed indentation

--- helpers.py
+++ helpers.py
@@ -4,19 +4,24 @@ import json
 from config import user_agent_list
 
 def getUserDecks(username):
-        url = (
-            "https://api.moxfield.com/v2/users/" +
-            username + "/decks?pageNumber=1&pageSize=99999"
-        )
+    # set the url to include the specified username
+    url = (
+        "https://api.moxfield.com/v2/users/" +
+        username + "/decks?pageNumber=1&pageSize=99999"
+    )
 
-        print(f"Grabbing <{username}>'s public decks from " + url)
-        
-        r = requests.get(url, headers={'User-Agent': user_agent_list[random.randint(0, len(user_agent_list)-1)]})
-        j = json.loads(r.text)
-
-        ids = [item["publicId"] for item in j["data"]]
-        ids2 = open("id2.json", "w")
-        json.dump(ids, ids2)
-        deckIds = open("ids.json", "w")
-        json.dump(j, deckIds)
-        return j
+    print(f"Grabbing <{username}>'s public decks from " + url)
+    
+    # send a request to get the information of a moxfield user
+    r = requests.get(url, headers={'User-Agent': user_agent_list[random.randint(0, len(user_agent_list)-1)]})
+    # load the response as a json
+    j = json.loads(r.text)
+    # extract all publicId's of decks associated with the user
+    ids = [item["publicId"] for item in j["data"]]
+    # store those id's in their own json
+    ids2 = open("id2.json", "w")
+    json.dump(ids, ids2)
+    # store the full json information from the original request
+    deckIds = open("ids.json", "w")
+    json.dump(j, deckIds)
+    return j
